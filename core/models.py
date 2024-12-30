542a7572fd28a31358c779f20eecb27870915e49
AidaKB <aida.beheshti81@gmail.com>
2024-12-31 00:33:00 +0330

    EscapeRoomQuestionCreateAPIView

--- core/models.py
+++ core/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 
-# Create your models here.
+class EscapeRoomQuestion(models.Model):
+    name = models.CharField(max_length=255, verbose_name="نام", unique=True)
+    description = models.TextField(verbose_name="توضیحات")
 
-class Team(models.Model):
-    team_name = models.CharField(max_length=255)
+    floor = models.IntegerField(verbose_name="طبقه")
+    x_coordinate = models.CharField(max_length=255, verbose_name="مختصات طولی")
+    y_coordinate = models.CharField(max_length=255, verbose_name="مختصات عرضی")
+
+    score = models.IntegerField(verbose_name="امتیاز")
+    answer_limitation = models.IntegerField(verbose_name="محدودیت تعداد پاسخ ها")
+    flag = models.CharField(max_length=255, verbose_name="پرچم")
+    coin = models.IntegerField(verbose_name="مقدار سکه")
+
+    # ToDo : add creator
+    created_at = models.DateTimeField(auto_now_add=True)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from core import serializers
+from rest_framework.permissions import IsAdminUser
+from core import models
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+
+class EscapeRoomQuestionCreateAPIView(generics.ListCreateAPIView):
+    queryset = models.EscapeRoomQuestion.objects.all()
+    serializer_class = serializers.EscapeRoomQuestionCreateSerializer
+    permission_classes = [IsAdminUser]
+
+    filter_backends = [SearchFilter]
+    search_fields = [
+        'id',
+        'name',
+        'description',
+        'floor',
+        'x_coordinate',
+        'y_coordinate',
+        'score',
+        'answer_limitation',
+        'flag',
+        'coin',
+        'created_at',
+    ]
+
