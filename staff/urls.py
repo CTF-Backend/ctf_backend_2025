80aad793236e3965346a074b2027d71a7298b7d7
MrAliM10 <civmes10v@gmail.com>
2025-02-14 13:00:17 +0330

    adding list create API view for clarification

--- staff/urls.py
+++ staff/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import ClarificationListCreateAPIView
 
 urlpatterns = [
-
+    path("clarifications/", ClarificationListCreateAPIView.as_view(), name='clarifications-list-create'),
 ]
