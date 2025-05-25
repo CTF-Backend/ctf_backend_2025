5d5fffd18311cb45220fa4d08684eb9119eecec7
AidaKB <aida.beheshti81@gmail.com>
2025-05-25 17:59:03 +0330

    Update settings.py

--- CTFBackend2025/settings.py
+++ CTFBackend2025/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
 
 # Application definition
 
